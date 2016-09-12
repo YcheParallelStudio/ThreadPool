115252b9039d50c98e92ea7e1bac5b2d737df4b1
cheyulin <yche@cse.ust.hk>
2016-09-12 15:41:13 +0800

    using namespace std

--- example.cpp
+++ example.cpp
@@ -4,25 +4,27 @@
 
 #include "ThreadPool.h"
 
+using namespace std;
+
 int main() {
 
     ThreadPool pool(4);
-    std::vector<std::future<int> > results;
+    vector<future<int>> results;
 
     for (int i = 0; i < 8; ++i) {
         results.emplace_back(
                 pool.enqueue([i] {
-                    std::cout << "hello " << i << std::endl;
-                    std::this_thread::sleep_for(std::chrono::seconds(1));
-                    std::cout << "world " << i << std::endl;
+                    cout << "hello " << i << endl;
+                    this_thread::sleep_for(chrono::seconds(1));
+                    cout << "world " << i << endl;
                     return i * i;
                 })
         );
     }
 
     for (auto &&result: results)
-        std::cout << result.get() << ' ';
-    std::cout << std::endl;
+        cout << result.get() << ' ';
+    cout << endl;
 
     return 0;
 }
